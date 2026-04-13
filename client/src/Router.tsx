38c972e396abf693411f2bb24137bfe72d60690e
nathan-nigussie <nathantolosa11@gmail.com>
2026-04-13 17:48:02 +0300

    update

--- client/src/Router.tsx
+++ client/src/Router.tsx
@@ -1,20 +1,24 @@
-import { Route, Switch } from "wouter";
+import { Route, Switch, Router } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
+
 import CarListing from "./pages/CarListing";
 import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import CarDetail from "./pages/CarDetail";
 import NotFound from "./pages/NotFound";
 
-function Router() {
+function AppRouter() {
   return (
-    <Switch>
-      <Route path="/" component={CarListing} />
-      <Route path="/about" component={AboutUs} />
-      <Route path="/contact" component={ContactUs} />
-      <Route path="/car/:id" component={CarDetail} />
-      <Route component={NotFound} />
-    </Switch>
+    <Router hook={useHashLocation}>
+      <Switch>
+        <Route path="/" component={CarListing} />
+        <Route path="/about" component={AboutUs} />
+        <Route path="/contact" component={ContactUs} />
+        <Route path="/car/:id" component={CarDetail} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
-export default Router;
+export default AppRouter;
